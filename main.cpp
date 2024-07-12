9bd65b4f2dba1d803ef64d80d26e76bb6b62c6a9
GC1A-TinLong <147121460+GC1A-TinLong@users.noreply.github.com>
2024-07-12 14:52:14 +0900

    finished

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <string>
+#include <iostream>
+#include <chrono>
+
+using namespace std;
 
 int main() {
 
+	std::string a(100000, 'a');
 
+	std::chrono::system_clock::time_point  start, end;
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	// 処理
+	std::string b = a;
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	cout << "コビー：" << chrono::duration_cast<chrono::microseconds>(end - start).count() << "μs" << endl;
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	// 処理
+	b = std::move(a);
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	cout << "移動：" << chrono::duration_cast<chrono::microseconds>(end - start).count() << "μs" << endl;
 
 	return 0;
 }
