91b79c40fabe1fddcfd318d8b47e3e7d515d08ff
GC1A-TinLong <147121460+GC1A-TinLong@users.noreply.github.com>
2024-07-12 14:23:37 +0900

    save

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <thread>
+
+static void Thread1() {
+	printf("thread 1\n");
+}
+
+static void Thread2() {
+	printf("thread 2\n");
+}
+
+static void Thread3() {
+	printf("thread 3\n");
+}
 
 int main() {
 
+	std::thread th1(Thread1);
+	th1.join();
 
+	std::thread th2(Thread2);
+	th2.join();
+
+	std::thread th3(Thread3);
+	th3.join();
 
 	return 0;
 }
